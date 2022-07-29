159f55e4f50f3155f5b02d265875497e29856d04
wdse <21ypompei@wakatipu.school.nz>
2022-07-30 00:49:20 +1200

    Yes javascrip!

---
+++ main.js
@@ -0,0 +1,60 @@
+const reviews = 
+[/* html */`<span class='title'>Hidden Figures</span>
+<p class='reviewText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Suspendisse eu est ullamcorper, auctor libero quis, condimentum quam. 
+Duis nulla neque, venenatis blandit consectetur sed, tincidunt nec tellus. 
+Praesent sed nibh sit amet purus cursus dapibus. 
+Etiam sit amet lacus aliquam, lacinia elit aliquet, feugiat leo. 
+Aliquam non justo at turpis vestibulum imperdiet quis non lacus. 
+Vivamus sollicitudin dolor urna, quis pulvinar risus euismod at. 
+Sed tellus ante, mattis at dapibus pulvinar, semper sit amet velit. 
+Proin lacus arcu, lobortis vel lacinia non, egestas eu ipsum.</p>`,
+// 
+/* html */`<span class='title'>Angel has Fallen</span>
+<p class='reviewText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Suspendisse eu est ullamcorper, auctor libero quis, condimentum quam. 
+Duis nulla neque, venenatis blandit consectetur sed, tincidunt nec tellus. 
+Praesent sed nibh sit amet purus cursus dapibus. 
+Etiam sit amet lacus aliquam, lacinia elit aliquet, feugiat leo. 
+Aliquam non justo at turpis vestibulum imperdiet quis non lacus. 
+Vivamus sollicitudin dolor urna, quis pulvinar risus euismod at. 
+Sed tellus ante, mattis at dapibus pulvinar, semper sit amet velit. 
+Proin lacus arcu, lobortis vel lacinia non, egestas eu ipsum.</p>`,
+// 
+/* html */`<span class='title'>The Gray Man</span>
+<p class='reviewText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Suspendisse eu est ullamcorper, auctor libero quis, condimentum quam. 
+Duis nulla neque, venenatis blandit consectetur sed, tincidunt nec tellus. 
+Praesent sed nibh sit amet purus cursus dapibus. 
+Etiam sit amet lacus aliquam, lacinia elit aliquet, feugiat leo. 
+Aliquam non justo at turpis vestibulum imperdiet quis non lacus. 
+Vivamus sollicitudin dolor urna, quis pulvinar risus euismod at. 
+Sed tellus ante, mattis at dapibus pulvinar, semper sit amet velit. 
+Proin lacus arcu, lobortis vel lacinia non, egestas eu ipsum.</p>`];
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function debug(type,text) {
+    console.log('\n-------------------------------------------------');
+    console.log('[Debug | ' + type + '] : ' + text);
+    console.log('-------------------------------------------------');
+};
+
+
+debug('Page Load','Starting...',false);
+
+var chosenMovie = 0;
+
+var reviewContentVar = document.querySelector('.reviewContent');
+
+reviewContentVar.innerHTML = reviews[0];
+
+
+function changeContent() {
+    var content = document.querySelector('.optionsMovies').value;
+    debug('Var value',content,false);
+    debug('HTML Being served',reviews[content],false)
+    reviewContentVar.innerHTML = reviews[content];
+};
