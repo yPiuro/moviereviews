1fcf850b6f78abb5a154c0066adbe16d407f8411
wdse <95382758+yPiuro@users.noreply.github.com>
2022-08-08 20:10:03 +1200

    Update form.js

--- form/form.js
+++ form/form.js
@@ -8,3 +8,27 @@ function OnInput() {
     this.style.height = "auto";
     this.style.height = (this.scrollHeight) + "px";
 }
+
+function clearForm() {
+    document.getElementById("senderEmailInput").value = null
+    document.getElementById("movieTitleInput").value = null
+    document.getElementById("reviewChangesInput").value = null
+}
+
+async function sendForm() {
+    const title = document.getElementById("movieTitleInput").value;
+    const name = document.getElementById("senderEmailInput").value;
+    const content = document.getElementById("reviewChangesInput").value;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://api.piuroprauxy.ml/form", true);
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.send(JSON.stringify({
+        email: [name],
+        "body": {
+            "title": title,
+            "name": name,
+            "content": content
+        }
+    }));
+    const response = xhr.response;
+}
